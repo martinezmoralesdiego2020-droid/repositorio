6ea9bc50a779345be47d1e4982045444a296bf1d
Vianey Guerrero <vianey.guerreroreyes@gmail.com>
2026-05-12 13:27:35 -0600

    agregue cambios

--- codigo1.py
+++ codigo1.py
@@ -1,2 +1,11 @@
 print("holaa")
 # cambio para actividad ssh
+
+# cambio para actividad de fork y pull request
+nombre = input("¿Cómo te llamas? ")
+edad = int(input("¿Cuántos años tienes? "))
+
+if edad >= 18:
+    print("Hola", nombre, ", eres mayor de edad.")
+else:
+    print("Hola", nombre, ", eres menor de edad.")
